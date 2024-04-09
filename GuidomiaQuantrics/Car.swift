056ee31724989d62f2840ea52f4985974a1fe60e
Emmanuel Salonga <devfrnzy@gmail.com>
2024-04-10 02:30:41 +0800

    Add Car model

---
+++ GuidomiaQuantrics/Car.swift
@@ -0,0 +1,18 @@
+//
+//  Car.swift
+//  GuidomiaQuantrics
+//
+//  Created by Emmanuel Salonga on 4/10/24.
+//
+
+import Foundation
+
+struct Car: Codable {
+    var make: String
+    var model: String
+    var customerPrice: Double
+    var marketPrice:  Double
+    var prosList: [String]
+    var consList: [String]
+    var rating: Int
+}
