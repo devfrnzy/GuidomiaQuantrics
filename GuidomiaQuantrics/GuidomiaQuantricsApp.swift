9743210011501b91e4352022616b72d97bac58c1
Emmanuel Salonga <devfrnzy@gmail.com>
2024-04-10 01:23:16 +0800

    Initial Commit

---
+++ GuidomiaQuantrics/GuidomiaQuantricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidomiaQuantricsApp.swift
+//  GuidomiaQuantrics
+//
+//  Created by Emmanuel Salonga on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidomiaQuantricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
